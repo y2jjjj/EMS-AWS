f6e681fec8ba7b8b7d7bd93267c43ae5098743ca
Yuvraj <135434326+y2jjjj@users.noreply.github.com>
2025-08-26 02:22:32 +0530

    Update EmpApp.py

--- EmpApp.py
+++ EmpApp.py
@@ -15,18 +15,21 @@ db_conn = connections.Connection(
     user=customuser,
     password=custompass,
     db=customdb
-)
 
+)
 output = {}
 table = 'employee'
+
 
 @app.route("/", methods=['GET', 'POST'])
 def home():
     return render_template('AddEmp.html')
 
+
 @app.route("/about", methods=['POST'])
 def about():
     return render_template('www.intellipaat.com')
+
 
 @app.route("/addemp", methods=['POST'])
 def AddEmp():
@@ -37,18 +40,18 @@ def AddEmp():
     location = request.form['location']
     emp_image_file = request.files['emp_image_file']
 
-    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s, %s)"
+    insert_sql = "INSERT INTO employee VALUES (%s, %s, %s, %s, %s)"
     cursor = db_conn.cursor()
 
     if emp_image_file.filename == "":
         return "Please select a file"
 
     try:
-        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location, emp_image_file))
+
+        cursor.execute(insert_sql, (emp_id, first_name, last_name, pri_skill, location))
         db_conn.commit()
         emp_name = "" + first_name + " " + last_name
-
-        # Upload image file in S3
+        # Uplaod image file in S3 #
         emp_image_file_name_in_s3 = "emp-id-" + str(emp_id) + "_image_file"
         s3 = boto3.resource('s3')
 
@@ -63,21 +66,20 @@ def AddEmp():
             else:
                 s3_location = '-' + s3_location
 
-            object_url = "https://s3{}.amazonaws.com/{}/{}".format(
+            object_url = "https://s3{0}.amazonaws.com/{1}/{2}".format(
                 s3_location,
                 custombucket,
-                emp_image_file_name_in_s3
-            )
+                emp_image_file_name_in_s3)
+
         except Exception as e:
             return str(e)
-        finally:
-            cursor.close()
 
-        print("All modification done...")
-        return render_template('AddEmpOutput.html', name=emp_name)
+    finally:
+        cursor.close()
 
-    except Exception as e:
-        return str(e)
+    print("all modification done...")
+    return render_template('AddEmpOutput.html', name=emp_name)
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=80, debug=True)
